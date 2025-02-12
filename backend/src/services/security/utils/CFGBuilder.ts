fc08dd6810ee9556158506db775b2540f390f660
solanasynthai <aexamo@yahoo.com>
2025-02-12 11:43:05 +0000

    Create CFGBuilder.ts

---
+++ backend/src/services/security/utils/CFGBuilder.ts
@@ -0,0 +1,8 @@
+import { Node, Expression } from '../types';
+
+export class CFGBuilder {
+  public buildFromNode(node: Node): any {
+    // Implementation for Control Flow Graph building
+    return {};
+  }
+}
